a09146d3bf7a16e71484124b009db0b8d94629a5
Sarosh <saroshabdullah2013@gmail.com>
2021-07-22 11:50:16 +0530

    Used useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // useEffect is a hook which works in a way that it takes two arguments one is the function that has to run
+  // when the dependency is changed and the other one is the dependency.
+
+  useEffect(() => {
+    const storedUserLoggedInInfo = localStorage.getItem("isLoggedIn");
+    // We are checking if that local storage contains an item called isLoggedIn and storing it into a constant variable.
+    // after that we are checking if it's value is equal to one or not.
+    if (storedUserLoggedInInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    // This will create a key value pair in our local storage of browser.
     setIsLoggedIn(true);
   };
 
